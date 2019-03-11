77e26fe66f2859884fcdb00c2e4dbad5e7876479
Lal <abhisheklalnediya@gmail.com>
2019-03-11 12:35:10 +0530

    shell

---
+++ src/search/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Seach extends Component {
+  render() {
+    return (
+      <div>
+          Search BOX
+      </div>
+    )
+  }
+}
